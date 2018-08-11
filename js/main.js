3ed881bec7c14eb55d5b8efec9c97974b7a162a9
EverlynAsiko <asikoeverlyn@gmail.com>
2018-08-11 09:12:37 +0300

    Added images.

--- js/main.js
+++ js/main.js
@@ -1,101 +1,165 @@
-/*
- * Create a list that holds all of your cards
- */
+// Memory Game
+// © 2014 Nate Wiley
+// License -- MIT
+// best in full screen, works on phones/tablets (min height for game is 500px..) enjoy ;)
+// Follow me on Codepen
 
+(function(){
+	
+	var Memory = {
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-const memory_array=['A','A','B','B','C','C','D','D','E','E','F','F','G','G','H','H'];
-let memory_values=[];
-let memory_tiles_ids=[];
-let tiles_flipped=0;
+		init: function(cards){
+			this.$game = $(".game");
+			this.$modal = $(".modal");
+			this.$overlay = $(".modal-overlay");
+			this.$restartButton = $("button.restart");
+			this.cardsArray = $.merge(cards, cards);
+			this.shuffleCards(this.cardsArray);
+			this.setup();
+		},
 
-// Shuffle function from http://stackoverflow.com/a/2450976
-/*function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+		shuffleCards: function(cardsArray){
+			this.$cards = $(this.shuffle(this.cardsArray));
+		},
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+		setup: function(){
+			this.html = this.buildHTML();
+			this.$game.html(this.html);
+			this.$memoryCards = $(".card");
+			this.paused = false;
+     	this.guess = null;
+			this.binding();
+		},
 
-    return array;
-}*/
-Array.prototype.memory_tile_shuffle=function(){
-	let i=this.length,j,temp;
-	while(--i>0){
-		j=Math.floor(Math.random() * (i+1));
-		temp=this[j];
-		this[j]=this[i];
-		this[i]=temp;
-	}
-}
-
-function newGamebox(){
-	tiles_flipped=0;
-	let output='';
-	memory_array.memory_tile_shuffle();
-	for (let i = 0; i < memory_array.length; i++) {
-		output+=`<div id="tile_"${i} onclick="memoryFlipTile(this,\'${memory_array[i]}\')"></div>`;
-	}
-	document.getElementById('gamebox').innerHTML=output;
-}
-
-function memoryFlipTile(tile, val){
-	if(tile.innerHTML===""&& memory_values.length<2){
-		tile.style.background='#FFF';
-		tile.innerHTML=val;
-		if(memory_values.length===0){
-			memory_values.push(val);
-			memory_tiles_ids.push(tile.id);
-		}else if(memory_values.length===1){
-			memory_values.push(val);
-			memory_tiles_ids.push(tile.id);
-			if(memory_values[0]===memory_values[1]){
-				tiles_flipped+=2;
-				//clear both arrays
-				memory_values=[];
-				memory_tiles_ids=[];
-				//check to see if the whole board is cleared
-				if(tiles_flipped===memory_array.length){
-					alert("Board cleared...generating new board");
-					document.getElementById('gamebox').innerHTML="";
-					newGamebox();
+		binding: function(){
+			this.$memoryCards.on("click", this.cardClicked);
+			this.$restartButton.on("click", $.proxy(this.reset, this));
+		},
+		// kinda messy but hey
+		cardClicked: function(){
+			var _ = Memory;
+			var $card = $(this);
+			if(!_.paused && !$card.find(".inside").hasClass("matched") && !$card.find(".inside").hasClass("picked")){
+				$card.find(".inside").addClass("picked");
+				if(!_.guess){
+					_.guess = $(this).attr("data-id");
+				} else if(_.guess == $(this).attr("data-id") && !$(this).hasClass("picked")){
+					$(".picked").addClass("matched");
+					_.guess = null;
+				} else {
+					_.guess = null;
+					_.paused = true;
+					setTimeout(function(){
+						$(".picked").removeClass("picked");
+						Memory.paused = false;
+					}, 600);
 				}
-			}else {
-				function flip2Back(){
-					let tile_1=document.getElementById(memory_tiles_ids[0]);
-					let tile_2=document.getElementById(memory_tiles_ids[1]);
-					tile_1.style.background='url(./images/cardtop.jpg) no-repeat';
-					tile_1.innerHTML="";
-					tile_2.style.background='url(./images/cardtop.jpg) no-repeat';
-					tile_2.innerHTML="";
-					//clear both arrays
-					memory_values=[];
-					memory_tiles_ids=[];
+				if($(".matched").length == $(".card").length){
+					_.win();
 				}
-				setTimeout(flip2Back, 700);
 			}
+		},
+
+		win: function(){
+			this.paused = true;
+			setTimeout(function(){
+				Memory.showModal();
+				Memory.$game.fadeOut();
+			}, 1000);
+		},
+
+		showModal: function(){
+			this.$overlay.show();
+			this.$modal.fadeIn("slow");
+		},
+
+		hideModal: function(){
+			this.$overlay.hide();
+			this.$modal.hide();
+		},
+
+		reset: function(){
+			this.hideModal();
+			this.shuffleCards(this.cardsArray);
+			this.setup();
+			this.$game.show("slow");
+		},
+
+		// Fisher--Yates Algorithm -- https://bost.ocks.org/mike/shuffle/
+		shuffle: function(array){
+			var counter = array.length, temp, index;
+	   	// While there are elements in the array
+	   	while (counter > 0) {
+        	// Pick a random index
+        	index = Math.floor(Math.random() * counter);
+        	// Decrease counter by 1
+        	counter--;
+        	// And swap the last element with it
+        	temp = array[counter];
+        	array[counter] = array[index];
+        	array[index] = temp;
+	    	}
+	    	return array;
+		},
+
+		buildHTML: function(){
+			var frag = '';
+			this.$cards.each(function(k, v){
+				frag += '<div class="card" data-id="'+ v.id +'"><div class="inside">\
+				<div class="front"><img src="'+ v.img +'"\
+				alt="'+ v.name +'" /></div>\
+				<div class="back"><img src="images/cardtop.jpg"\
+				alt="Match" /></div></div>\
+				</div>';
+			});
+			return frag;
 		}
-	}
-}
-window.addEventListener("load", newGamebox());
+	};
+
+	var cards = [
+		{
+			name: "burundi",
+			img: "images/burundi.jpeg",
+			id: 1
+		},
+		{
+			name: "djibouti",
+			img: "images/djibouti.jpeg",
+			id: 2
+		},
+		{
+			name: "eritrea",
+			img: "images/eritrea.jpeg",
+			id: 3
+		},
+		{
+			name: "ethiopia",
+			img: "images/ethiopia.jpeg",
+			id: 4
+		}, 
+		{
+			name: "kenya",
+			img: "images/kenya.jpg",
+			id: 5
+		},
+		{
+			name: "rwanda",
+			img: "images/rwanda.jpeg",
+			id: 6
+		},
+		{
+			name: "tanzania",
+			img: "images/tanzania.jpeg",
+			id: 7
+		},
+		{
+			name: "uganda",
+			img: "images/uganda.jpg",
+			id: 8
+		},
+	];
+    
+	Memory.init(cards);
 
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+})();
