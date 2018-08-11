4ba0001efa995ff23b7c0ec1ab95a1faced43d7e
EverlynAsiko <asikoeverlyn@gmail.com>
2018-08-11 21:50:02 +0300

    added timer,move counter, star icons and restart button

--- js/main.js
+++ js/main.js
@@ -1,14 +1,3 @@
-/*
- * Create a list that holds all of your cards
- */
-
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
 let memory_array = ['images/burundi.jpeg','images/burundi.jpeg','images/djibouti.jpeg',
 'images/djibouti.jpeg','images/eritrea.jpeg','images/eritrea.jpeg','images/ethiopia.jpeg',
 'images/ethiopia.jpeg','images/kenya.jpg','images/kenya.jpg','images/rwanda.jpeg',
@@ -16,22 +5,38 @@ let memory_array = ['images/burundi.jpeg','images/burundi.jpeg','images/djibouti
 'images/uganda.jpg'];
 let memory_values = [];
 let memory_tile_ids = [];
-let tiles_flipped = 0;;
+let tiles_flipped = 0;
+let moveCount=0;
+let clicked = false;
+let sec = 0;
+let minute=0;
+
+
+function startClock() {
+    if (clicked === false) {
+        clock = setInterval("stopWatch()", 1000);
+        clicked = true;
+    }
+}
+
+function stopWatch() {
+    sec++;
+    if (sec===60){
+    	minute++;
+    	sec=0;
+    }
+    document.getElementById("timer").innerHTML = minute+"."+sec;
+}
+
+function stopClock() {
+    sec = 0;
+    minute=0;
+    document.getElementById("timer").innerHTML= minute+"."+sec;
+    clicked = false;
+}
+
 
 // Shuffle function from http://stackoverflow.com/a/2450976
-/*function shuffle(array) {
-    let currentIndex = array.length, temporaryValue, randomIndex;
-
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-
-    return array;
-}*/
 Array.prototype.memory_tile_shuffle = function(){
     let i = this.length, j, temp;
     while(--i > 0){
@@ -50,9 +55,15 @@ function newBoard(){
 		output += '<div id="tile_'+i+'" onclick="memoryFlipTile(this,\'<img src='+memory_array[i]+'>\')"></div>';
 	}
 	document.getElementById('memory_board').innerHTML = output;
+	moveCount=0;
+	document.getElementById('moves').innerHTML=moveCount;
+	stopClock();
 }
 
 function memoryFlipTile(tile,val){
+	moveCount++;
+	startClock();
+	rating();
 	if(tile.innerHTML == "" && memory_values.length < 2){
 		tile.innerHTML = val;
 		if(memory_values.length == 0){
@@ -68,9 +79,11 @@ function memoryFlipTile(tile,val){
             	memory_tile_ids = [];
 				// Check to see if the whole board is cleared
 				if(tiles_flipped == memory_array.length){
-					alert("Board cleared... generating new board");
+					alert("YOU ARE ON FIRE!... Play again?");
 					document.getElementById('memory_board').innerHTML = "";
 					newBoard();
+					moveCount=0;
+					stopClock();
 				}
 			} else {
 				function flip2Back(){
@@ -89,17 +102,24 @@ function memoryFlipTile(tile,val){
 			}
 		}
 	}
+	document.getElementById('moves').innerHTML=moveCount;
 }
+
+function rating(){
+	let star=[document.getElementById("rating").childNodes[0].nodeValue,
+	document.getElementById("rating").childNodes[1].nodeValue,
+	document.getElementById("rating").childNodes[2].nodeValue,
+	document.getElementById("rating").childNodes[3].nodeValue,
+	document.getElementById("rating").childNodes[4].nodeValue,];
+	if (moves > 8 && moves < 12){
+		star[4].style.visibility = "hidden";
+    }
+    else if (moves > 13){
+    	star[3].style.visibility = "hidden";
+    }
+}
+
+
 window.addEventListener("load", newBoard());
 
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
