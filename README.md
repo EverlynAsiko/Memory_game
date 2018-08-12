ccf7e2a79172c8ad09dd779781916cea0f0024cd
EverlynAsiko <asikoeverlyn@gmail.com>
2018-08-12 18:32:48 +0300

    Corrected syntax errors and resolve javascript bugs

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # Memory_game
 
-This is a front end udacity project. 
+This is a front end nanodegree udacity project. 
 
 **How it works**
 
-When the window loads so does the game board. The time starts when the first card is clicked as well as the moves counter._moves is counted in form of clicks_
-One can restart the game this will set the timer and moves counter to 0 and reload a new board.
+When the window loads so does the game board. The timer starts when the first card is clicked as well as the moves counter._Moves are counted in form of clicks_
+One can restart the game,this will set the timer and moves counter to 0 and reload a new game board.
 
-When one matches a set of cards the disappear from the game board and when the cards are not a match they flip back.
+When one matches a set of cards they disappear from the game board and when the cards are not a match they flip back.
 
 For one to get a good rating you have to be fast that is use as less time as possible and use the least amount of clicks as possible.
 
--- js/main.js
+++ js/main.js
@@ -6,19 +6,21 @@ let memory_array = ['images/burundi.jpeg','images/burundi.jpeg','images/djibouti
 let memory_values = [];
 let memory_tile_ids = [];
 let tiles_flipped = 0;
-let moveCount=0;
+let moveCount = 0;
 let clicked = false;
 let sec = 0;
-let minute=0;
+let minute = 0;
+let stars=5;
 
-
+//Starts timer
 function startClock() {
-    if (clicked === false) {
-        clock = setInterval("stopWatch()", 1000);
-        clicked = true;
-    }
+	if (clicked === false) {
+		clock = setInterval("stopWatch()", 1000);
+		clicked = true;
+	}
 }
 
+//stops timer
 function stopWatch() {
     sec++;
     if (sec===60){
@@ -28,6 +30,7 @@ function stopWatch() {
     document.getElementById("timer").innerHTML = minute+"."+sec;
 }
 
+//restarts timer
 function stopClock() {
     sec = 0;
     minute=0;
@@ -35,27 +38,27 @@ function stopClock() {
     clicked = false;
 }
 
+//Star rating dependancies
 function rating(){
-		stars=5;
-		if (moveCount > 16 && moveCount <= 24){
-			document.getElementById('star5').style.visibility = "hidden";
-			stars=4;
-	    }
-	    else if (moveCount > 24 && moveCount <= 32){
-	    	document.getElementById('star4').style.visibility = "hidden";
-	    	stars=3;
-	    }
-	    else if (moveCount > 32 && moveCount <= 48){
-	    	document.getElementById('star3').style.visibility = "hidden";
-	    	stars=2;
-	    }
-	    else if (moveCount > 48){
-	    	document.getElementById('star2').style.visibility = "hidden";
-	    	stars=1;
-	    }
+	if (moveCount > 32 && moveCount <= 48 && sec > 30){
+		document.getElementById('star5').style.visibility = "hidden";
+		stars=4;
 	}
+	else if (moveCount > 48 && moveCount <= 64){
+		document.getElementById('star4').style.visibility = "hidden";
+		stars=3;
+	}
+	else if (moveCount > 64 && moveCount <= 80){
+		document.getElementById('star3').style.visibility = "hidden";
+		stars=2;
+	}
+	else if (moveCount > 80){
+		document.getElementById('star2').style.visibility = "hidden";
+		stars=1;
+	}
+}
 
-// Shuffle function from http://stackoverflow.com/a/2450976
+// Shuffle function dynamically shuffling the board
 Array.prototype.memory_tile_shuffle = function(){
     let i = this.length, j, temp;
     while(--i > 0){
@@ -66,39 +69,48 @@ Array.prototype.memory_tile_shuffle = function(){
     }
 }
 
+//creates a new game board and restarts every variable
 function newBoard(){
 	tiles_flipped = 0;
 	let output = '';
     memory_array.memory_tile_shuffle();
 	for(let i = 0; i < memory_array.length; i++){
-		output += '<div id="tile_'+i+'" onclick="memoryFlipTile(this,\'<img src='+memory_array[i]+'>\')"></div>';
+		output += '<div class="flipper" id="tile_'+i+'" onclick="memoryFlipTile(this,\'<img src='+memory_array[i]+'>\')"></div>';
 	}
 	document.getElementById('memory_board').innerHTML = output;
 	moveCount=0;
 	document.getElementById('moves').innerHTML=moveCount;
 	stopClock();
+	document.getElementById('star2').style.visibility = "visible";
+	document.getElementById('star3').style.visibility = "visible";
+	document.getElementById('star4').style.visibility = "visible";
+	document.getElementById('star5').style.visibility = "visible";
+	stars=5;
+
 }
 
+//called when card is flipped
 function memoryFlipTile(tile,val){
 	moveCount++;
 	startClock();
 	rating();
 	
-	if(tile.innerHTML == "" && memory_values.length < 2){
+	if(tile.innerHTML === "" && memory_values.length < 2){
 		tile.innerHTML = val;
-		if(memory_values.length == 0){
+		if(memory_values.length === 0){
 			memory_values.push(val);
 			memory_tile_ids.push(tile.id);
-		} else if(memory_values.length == 1){
+		} else if(memory_values.length === 1){
 			memory_values.push(val);
 			memory_tile_ids.push(tile.id);
-			if(memory_values[0] == memory_values[1]){
+			//Compares the flipped tiles
+			if(memory_values[0] === memory_values[1]){
 				tiles_flipped += 2;
 				let tile_1 = document.getElementById(memory_tile_ids[0]);
 				let tile_2 = document.getElementById(memory_tile_ids[1]);
 				tile_1.classList.add("fade");
 				tile_2.classList.add("fade");
-				// Clear both arrays
+				// Clears the array for a fresh pair of flips
 				memory_values = [];
             	memory_tile_ids = [];
 				// Check to see if the whole board is cleared
@@ -110,21 +122,22 @@ function memoryFlipTile(tile,val){
 				}
 			} else {
 				function flip2Back(){
-				    // Flip the 2 tiles back over
+				    // Flip the 2 tiles back over when they do not match
 				    let tile_1 = document.getElementById(memory_tile_ids[0]);
 				    let tile_2 = document.getElementById(memory_tile_ids[1]);
-				    tile_1.classList.add("flipped");
-			        tile_2.classList.add("flipped");
+				    tile_1.classList.toggle("flipped");
+			        tile_2.classList.toggle("flipped");
 				    tile_1.style.background = 'url(images/cardtop.jpg) no-repeat';
 				    tile_1.style.backgroundSize = 'cover';
 				    tile_1.innerHTML = "";
 				    tile_2.style.background = 'url(images/cardtop.jpg) no-repeat';
 				    tile_2.style.backgroundSize = 'cover';
             	    tile_2.innerHTML = "";
+
 				    // Clear both arrays
 				    memory_values = [];
             	    memory_tile_ids = [];
-				}
+				};
 				setTimeout(flip2Back, 700);
 			}
 		}
