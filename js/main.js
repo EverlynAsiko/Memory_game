c37ab365ad1cabfc879b27d572180b1653f77d85
EverlynAsiko <asikoeverlyn@gmail.com>
2018-08-12 15:18:48 +0300

    Added styling and fading

--- js/main.js
+++ js/main.js
@@ -94,6 +94,10 @@ function memoryFlipTile(tile,val){
 			memory_tile_ids.push(tile.id);
 			if(memory_values[0] == memory_values[1]){
 				tiles_flipped += 2;
+				let tile_1 = document.getElementById(memory_tile_ids[0]);
+				let tile_2 = document.getElementById(memory_tile_ids[1]);
+				tile_1.classList.add("fade");
+				tile_2.classList.add("fade");
 				// Clear both arrays
 				memory_values = [];
             	memory_tile_ids = [];
@@ -112,7 +116,7 @@ function memoryFlipTile(tile,val){
 				    let tile_1 = document.getElementById(memory_tile_ids[0]);
 				    let tile_2 = document.getElementById(memory_tile_ids[1]);
 				    tile_1.style.background = 'url(images/cardtop.jpg) no-repeat';
-            	    tile_1.innerHTML = "";
+				    tile_1.innerHTML = "";
 				    tile_2.style.background = 'url(images/cardtop.jpg) no-repeat';
             	    tile_2.innerHTML = "";
 				    // Clear both arrays
